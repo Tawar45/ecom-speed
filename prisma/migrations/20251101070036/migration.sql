01175d17733200ac0da1c825c6529dc854ea7b2b
Shiv Dutt Chauhan <155214455+Shivdutt44@users.noreply.github.com>
2025-11-03 14:15:33 +0530

    org

---
+++ prisma/migrations/20251101070036/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `shop` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `isOnline` BOOLEAN NOT NULL DEFAULT false,
+    `scope` VARCHAR(191) NULL,
+    `expires` DATETIME(3) NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `userId` BIGINT NULL,
+    `firstName` VARCHAR(191) NULL,
+    `lastName` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `accountOwner` BOOLEAN NOT NULL DEFAULT false,
+    `locale` VARCHAR(191) NULL,
+    `collaborator` BOOLEAN NULL DEFAULT false,
+    `emailVerified` BOOLEAN NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Shop` (
+    `id` VARCHAR(191) NOT NULL,
+    `domain` VARCHAR(191) NOT NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Shop_domain_key`(`domain`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Subscription` (
+    `id` VARCHAR(191) NOT NULL,
+    `shopId` VARCHAR(191) NOT NULL,
+    `plan` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Subscription` ADD CONSTRAINT `Subscription_shopId_fkey` FOREIGN KEY (`shopId`) REFERENCES `Shop`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
