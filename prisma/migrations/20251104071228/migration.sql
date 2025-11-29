ed8e8ba14cb5f70dde6744c18e9c933e1f7a90d9
Rohit Tawar <rohit@thr1ve.me>
2025-11-29 13:05:18 +0530

    modify migration sql

--- prisma/migrations/20251104071228/migration.sql
+++ prisma/migrations/20251104071228/migration.sql
@@ -26,7 +26,7 @@ CREATE TABLE `Shop` (
     `accessToken` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-
+    `welcomeEmailSent` BOOLEAN NOT NULL DEFAULT false,
     UNIQUE INDEX `Shop_domain_key`(`domain`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20251104115831_fix_shop/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `shop` ADD COLUMN `welcomeEmailSent` BOOLEAN NOT NULL DEFAULT false;
